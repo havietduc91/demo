f273991874e6ff9de3fbc44e26da8ee3a99aad43
Ha Viet Duc <havietduc91@gmail.com>
2014-05-14 00:08:26 +0700

    add related video, new video home page

--- application/modules/site/controllers/IndexController.php
+++ application/modules/site/controllers/IndexController.php
@@ -3,19 +3,12 @@ class Site_IndexController extends Cl_Controller_Action_Index
 {
     public function indexAction()
     {
-        if($this->_u['id'] == GUEST_ID) {
-            Bootstrap::$pageTitle = "Your website title";
-            return;
+        $daoVideo = Dao_Node_Video::getInstance();
+        $r = $daoVideo->findAll();
+        if($r['success'] && $r['count'] > 0) {
+        	$this->setViewParam('list', $r['result']);
         }
-        else 
-        {
-            $daoSamx = Dao_Node_Samx::getInstance();
-            $r = $daoSamx->findAll();
-            if($r['success'] && $r['count'] > 0) {
-                $this->setViewParam('list', $r['result']);
-            }
-        }
-        Bootstrap::$pageTitle = "Site";
+        Bootstrap::$pageTitle = "Tổng hợp cover hay nhất, hài nhất";
     }
 	public function errorAction()
 	{
